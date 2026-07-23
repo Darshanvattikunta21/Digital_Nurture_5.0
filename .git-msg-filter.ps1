74f4318452da676fdbd0098ef801f3f9753a722e
Vattikunta Darshan <your-github-darshanvattikunta@gmail.com>
2026-07-23 13:49:15 +0530

    Remove git helper scripts

--- .git-msg-filter.ps1
+++
@@ -1,6 +0,0 @@
-switch ($env:GIT_COMMIT) {
-    '2a41205' { Write-Output 'Week1: Module1 Design Patterns exercises' ; break }
-    '2658651' { Write-Output 'Week2: Module2 DSA exercises' ; break }
-    'c1c8783' { Write-Output 'Week3: Spring Core and Spring Data JPA exercises' ; break }
-    default { Get-Content -Raw - }
-}
